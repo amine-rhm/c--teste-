c42a69accafd87d070988f763fa8aa622075cd81
RAHMOUNI MDA <rahmounimhdamine@gmail.com>
2025-12-29 01:36:35 +0100

    La couche entry points - 1.4

--- UniversiteRestApi/Program.cs
+++ UniversiteRestApi/Program.cs
@@ -1,10 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the controller.
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer(); 
 // Chargement des services Swagger
 builder.Services.AddSwaggerGen();
 
@@ -13,13 +11,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    // Démarrage du service de documentation de l'API au lancement du projet en dev
+    app.UseSwagger();
+    // Démarrage de l'interface graphique de Swagger au lancement du projet en dev
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
